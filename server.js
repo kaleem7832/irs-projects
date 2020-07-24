d25cdaea13e8ab5d28959c1982ba35505cb5f0cf
kaleem7832 <kaleem.nalband@gmail.com>
2020-07-24 18:33:22 +0530

    final setup for heroku launch

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const PORT = 4000;
+const PORT = process.env.PORT || 8000;
 const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./DB.js");
 const projectsRoutes = require("./projects.routes");
 const taskRoutes = require("./taskroutes");
+require("dotenv").config();
+
+// ... other imports
+const path = require("path");
+
+// ... other app.use middleware
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+// ...
+
 mongoose.Promise = global.Promise;
 mongoose
   .connect(config.DB, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -26,6 +36,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/projects", projectsRoutes);
 app.use("/tasks", taskRoutes);
+
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(PORT, function () {
   console.log("Server is running on Port:", PORT);
 });
